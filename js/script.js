03b94516ce0b2b795e3030320d1e63f6fac0d306
irina-aice <61356164+irina-aice@users.noreply.github.com>
2020-10-26 13:41:02 +0300

    Доработка проекта

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const searchPopup = document.querySelector('.search-popup');
 const searchForm = document.querySelector('.search-form');
 const searchAdult = document.querySelector('.search-form-adult');
 const searchChildren = document.querySelector('.search-form-children');
+const searchArrival = document.querySelector('.search-form-arrival-date');
+const searchDepature =document.querySelector('.search-form-departure-date');
 
 if (localStorage.getItem('adult')) {
   searchAdult.value = localStorage.getItem('adult');
@@ -18,8 +20,21 @@ buttonSearch.addEventListener('click', function () {
 });
 
 searchForm.addEventListener('submit', function(evt){
-  evt.preventDefault();
+  if (!searchArrival.value || !searchDepature.value) {
+    evt.preventDefault();
+    searchForm.classList.add('search-form-error');
+    return false;
+  }
 
-  localStorage.setItem('adult', searchAdult.value);
-  localStorage.setItem('children', searchChildren.value);
+  if (searchAdult.value) {
+    localStorage.setItem('adult', searchAdult.value);
+  }
+
+  if (searchChildren.value) {
+    localStorage.setItem('children', searchChildren.value);
+  }
+});
+
+searchForm.addEventListener('animationend', function(evt) {
+  searchForm.classList.remove('search-form-error');
 });
